bf6d102137fbc276cce29bf070cfdd4c624f6e66
damonluu <luudamon1996@gmail.com>
2017-11-20 18:50:26 -0800

    fix carpool, added some data.riderid == riderid checks to rider socket

--- app.js
+++ app.js
@@ -10,87 +10,77 @@ var db_connection = require('./api/controllers/db_connection');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.use(function(req, res, next)
-{
-    console.log(req.method, req.url);
-    next();
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
 });
 
 //define the port to host the server
 app.set('port', 1600);
 
 //middleware to console log every request
-var serverListener = function(req, res)
-{
-    console.log('Express server listening on port ' + app.get('port'));
+var serverListener = function(req, res) {
+  console.log('Express server listening on port ' + app.get('port'));
 };
 
 //set static directory before defining route
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public',
-{
-    extensions: ['html']
+app.use(express.static(__dirname + '/public', {
+  extensions: ['html']
 }));
 
 app.use(routes);
 
 var server = http.createServer(app).listen(app.get('port'), serverListener);
 
-
-
 //setting up google map api
-
-var googleMapsClient = require('@google/maps').createClient(
-{
-    key: 'AIzaSyDNIMuefOw8IFBBjGifWHAMMuSKOC7epj0'
-});
-
-googleMapsClient.distanceMatrix(
-{
-    origins: [
-    {
-        lat: 37.3352,
-        lng: -121.8811
-    }],
-    destinations: [
-    {
-        lat: 37.4323,
-        lng: -121.8996
-    }]
-}, function(err, response)
-{
-    if (!err)
-    {
-        console.log(response.json.rows[0].elements[0].distance.value);
-
-    }
-});
+//
+// var googleMapsClient = require('@google/maps').createClient(
+// {
+//     key: 'AIzaSyDNIMuefOw8IFBBjGifWHAMMuSKOC7epj0'
+// });
+//
+// googleMapsClient.distanceMatrix(
+// {
+//     origins: [
+//     {
+//         lat: 37.3352,
+//         lng: -121.8811
+//     }],
+//     destinations: [
+//     {
+//         lat: 37.4323,
+//         lng: -121.8996
+//     }]
+// }, function(err, response)
+// {
+//     if (!err)
+//     {
+//         console.log(response.json.rows[0].elements[0].distance.value);
+//
+//     }
+// });
 
 var io = require('socket.io')(server);
 //handling events happen on socket.io
 
-io.on('connection', function(socket)
-{
-    console.log('a client has connected');
-    //return the location of all active driver when the client emit 'map view'
-    socket.on('rider view', function(data)
-    {
-        db_connection.getConnection(function(err, c)
-        {
-            var querySelectDriver = 'SELECT * FROM Drivers'
-            c.query(querySelectDriver, function(err, result, feilds)
-            {
-                if (err) throw err;
-                c.release();
-                console.log(result);
-                io.emit('map view', result);
-
-            });
-        });
+io.on('connection', function(socket) {
+  console.log('a client has connected');
+  //return the location of all active driver when the client emit 'map view'
+  socket.on('rider view', function(data) {
+    db_connection.getConnection(function(err, c) {
+      var querySelectDriver = 'SELECT * FROM Drivers'
+      c.query(querySelectDriver, function(err, result, feilds) {
+        if (err) throw err;
+        c.release();
+        console.log(result);
+        io.emit('map view', result);
+      });
     });
+  });
 
-    //data should contain: driver id, current long, current lat
-     socket.on('new driver', function(data) {
+  //data should contain: driver id, current long, current lat
+  socket.on('new driver', function(data) {
     console.log('new driver available');
     console.log(data);
     db_connection.getConnection(function(err, c) {
@@ -103,183 +93,138 @@ io.on('connection', function(socket)
       c.release();
     });
   });
-     
 
-    //data for request ride: driver id, rider id, dest long, dest lat, start long, start lat, cost, carpool, time
-    //Add the first rider
-    socket.on('ride request', function(data)
-    {
-        console.log('ride request ....');
-        console.log(data);
-        db_connection.getConnection(function(err, c)
-        {
-            var queryInsertRide = 'INSERT INTO Rides VALUE (' + data.driverID + ', ' + data.riderID + ', ' + data.destinationLng + ', ' + data.destinationLat + ', ' + data.riderLng + ', ' + data.riderLat + ', ' + data.cost + ', ' + data.carpool + ', ' + data.duration + ')';
-            console.log(queryInsertRide);
-            c.query(queryInsertRide, function(err, result, feilds)
-            {
-                if (err) throw err;
-                console.log(result);
-            });
 
-            c.release();
+  //data for request ride: driver id, rider id, dest long, dest lat, start long, start lat, cost, carpool, time
+  //Add the first rider
+  socket.on('ride request', function(data) {
+    console.log('ride request ....');
+    console.log(data);
+    db_connection.getConnection(function(err, c) {
+      var queryInsertRide = 'INSERT INTO Rides VALUE (' + data.driverID + ', ' + data.riderID + ', ' + data.destinationLng + ', ' + data.destinationLat + ', ' + data.riderLng + ', ' + data.riderLat + ', ' + data.cost + ', ' + data.carpool + ', ' + data.duration + ')';
+      console.log(queryInsertRide);
+      c.query(queryInsertRide, function(err, result, feilds) {
+        if (err) throw err;
+        console.log(result);
+      });
 
-        });
-        console.log('notifying the driver');
+      c.release();
+
+    });
+    console.log('notifying the driver');
+    io.emit("new rider", data);
+    io.emit("update map");
+  });
+
+  //Remove the driver on both views once the driver clicks ride completed
+  socket.on('remove driver', function(data) {
+    console.log("Remove Driver: " + data.id);
+    db_connection.getConnection(function(err, c) {
+      var queryUpdateSeat = 'UPDATE DRIVERS SET AVAILABLESEATS = SEATS WHERE ID = ' + data.id;
+      console.log(queryUpdateSeat);
+      c.query(queryRemove, function(err, result, feilds) {
+        if (err) throw err;
+        console.log(result);
+      });
+
+      var queryRemove = 'DELETE FROM RIDES WHERE driverID = ' + data.id;
+      console.log(queryRemove);
+      c.query(queryRemove, function(err, result, feilds) {
+        if (err) throw err;
+        c.release();
+        console.log(result);
+        console.log('removed successfully');
+        io.emit("ride completed", result);
+      });
+    });
+  });
+
+  //rework carpool to prioritize closest driver first
+  socket.on('find closest', function(data) {
+    console.log("************************************");
+    console.log("inside find closest appjs, data is below");
+    console.log(data);
+    db_connection.getConnection(function(err, c) {
+      var querySelectRides = 'select * from rides x where exists (select count(*) from rides group by driverid having driverid = x.driverid and count(*)<2)'
+      c.query(querySelectRides, function(err, result, feilds) {
+        if (err) throw err;
+        c.release();
+        console.log("************************************");
+        console.log("inside find closest appjs line 203, result is below");
+        console.log(result);
+        console.log(result.length > 0);
+        if (result.length == 0) {
+          console.log("no active ride (find closest socket)");
+          io.emit('find nearest', data);
+        } else {
+          console.log("look for carpool");
+          console.log(result);
+          result[0]["carpoolRequesterId"] = data.riderID;
+          console.log("**********************************");
+          console.log("SETTING CARPOOL REQUEST ID");
+          console.log(result);
+          io.emit('search carpool', result);
+        }
+      })
+    });
+  });
+
+  //approach 2: update markers
+  socket.on('update seats db', function(data) {
+    console.log("UPDATE SEATS DB" + data);
+    db_connection.getConnection(function(err, c) {
+      var queryUpdateSeat = 'UPDATE Drivers SET AVAILABLESEATS = ' + data[3] + ' WHERE ID = ' + data[0];
+      console.log(queryUpdateSeat);
+      c.query(queryUpdateSeat, function(err, result, fields) {
+        if (err) throw err;
+        c.release();
+        io.emit('update seats', data);
+        console.log(result);
+      });
+    });
+  });
+
+  socket.on('notify carpool', function(data) {
+    console.log("app js notify carpool");
+    console.log(data);
+
+    db_connection.getConnection(function(err, c) {
+      var queryUpdateCarpool = 'UPDATE Rides SET carpool=TRUE, cost=cost-5 WHERE driverid=' + data.driverID;
+      c.query(queryUpdateCarpool, function(err, result, feilds) {
+        if (err) throw err;
+        console.log(result);
+      });
+
+      var queryInsertRide = 'INSERT INTO Rides VALUE (' + data.driverID + ', ' + data.riderID + ', ' + data.destinationLng + ', ' + data.destinationLat + ', ' + data.riderLng + ', ' + data.riderLat + ', ' + data.cost + ', ' + data.carpool + ', ' + data.duration + ')';
+      console.log(queryInsertRide);
+      c.query(queryInsertRide, function(err, result, feilds) {
+        if (err) throw err;
+        console.log(result);
         io.emit("new rider", data);
-        io.emit("update map");
-    });
+      });
 
-    //Remove the driver on both views once the driver clicks ride completed
-    //Remove the driver on both views once the driver clicks ride completed
-    socket.on('remove driver', function(data)
-    {
-        console.log("Remove Driver: " + data.id);
-        db_connection.getConnection(function(err, c)
-        {
-            var queryUpdateSeat = 'UPDATE DRIVERS SET AVAILABLESEATS = SEATS WHERE ID = ' + data.id;
-            console.log(queryUpdateSeat);
-            c.query(queryRemove, function(err, result, feilds)
-            {
-                if (err) throw err;
-                console.log(result);
-            });
+      var queryGetRider = 'SELECT riderid FROM Rides WHERE driverid=' + data.driverID;
+      c.query(queryGetRider, function(err, result, feilds) {
+        if (err) throw err;
+        console.log(result);
+        var rider1 = result[0].riderid;
+        var rider2 = result[1].riderid;
 
-            var queryRemove = 'DELETE FROM RIDES WHERE driverID = ' + data.id;
-            console.log(queryRemove);
-            c.query(queryRemove, function(err, result, feilds)
-            {
-                if (err) throw err;
-                c.release();
-                console.log(result);
-                console.log('removed successfully');
-                io.emit("ride completed", result);
-            });
-        });
+        var returnData = {
+          'driverID': data.driverID,
+          'rider1': rider1,
+          'rider2': rider2
+        };
+        console.log(returnData);
+        io.emit('second rider', returnData);
+      });
+      c.release();
+    })
+  });
 
-    });
-
-    //data: riderId, rider-lat, rider-long, dest-lat, dest-long
-    // socket.on('active rides', function(data)
-    // {
-    //     console.log(data);
-    //     db_connection.getConnection(function(err, c)
-    //     {
-    //      var querySelectRides = 'select * from rides x where exists (select count(*) from rides group by driverid having driverid = x.driverid and count(*)<2)'
-    //         c.query(querySelectRides, function(err, result, feilds)
-    //         {
-    //             if (err) throw err;
-    //             c.release();
-    //             console.log(result);
-    //             console.log(result.length > 0);
-    //             if (result.length == 0)
-    //             {
-    //                 console.log("no active ride");
-    //                 io.emit('find nearest', data);
-    //             }
-    //             else
-    //             {
-    //                 console.log("look for carpool");
-    //                 console.log(result);
-    //                 io.emit('search carpool', result);
-    //             }
-    //         })
-    //     });
-    // });
-
-    //rework carpool to prioritize closest driver first
-    socket.on('find closest', function(data)
-    {
-        console.log(data);
-        db_connection.getConnection(function(err, c)
-        {
-            var querySelectRides = 'select * from rides x where exists (select count(*) from rides group by driverid having driverid = x.driverid and count(*)<2)'
-            c.query(querySelectRides, function(err, result, feilds)
-            {
-                if (err) throw err;
-                c.release();
-                console.log(result);
-                console.log(result.length > 0);
-                if (result.length == 0)
-                {
-                    console.log("no active ride (find closest socket)");
-                    io.emit('find nearest', data);
-                }
-                else
-                {
-                    console.log("look for carpool");
-                    console.log(result);
-                    io.emit('search carpool', result);
-                }
-            })
-        });
-    });
-
-    //approach 2: update markers
-    socket.on('update seats db', function(data)
-    {
-        console.log("UPDATE SEATS DB" + data);
-        db_connection.getConnection(function(err, c)
-        {
-            var queryUpdateSeat = 'UPDATE Drivers SET AVAILABLESEATS = ' + data[3] + ' WHERE ID = ' + data[0];
-            console.log(queryUpdateSeat);
-            c.query(queryUpdateSeat, function(err, result, fields)
-            {
-                if (err) throw err;
-                c.release();
-                io.emit('update seats', data);
-                console.log(result);
-            });
-        });
-    });
-
-    socket.on('notify carpool', function(data)
-    {
-        console.log("app js notify carpool");
-        console.log(data);
-
-        db_connection.getConnection(function(err, c)
-        {
-            var queryUpdateCarpool = 'UPDATE Rides SET carpool=TRUE, cost=cost-5 WHERE driverid=' + data.driverID;
-            c.query(queryUpdateCarpool, function(err, result, feilds)
-            {
-                if (err) throw err;
-                console.log(result);
-            });
-
-            var queryInsertRide = 'INSERT INTO Rides VALUE (' + data.driverID + ', ' + data.riderID + ', ' + data.destinationLng + ', ' + data.destinationLat + ', ' + data.riderLng + ', ' + data.riderLat + ', ' + data.cost + ', ' + data.carpool + ', ' + data.duration + ')';
-            console.log(queryInsertRide);
-            c.query(queryInsertRide, function(err, result, feilds)
-            {
-                if (err) throw err;
-                console.log(result);
-                io.emit("new rider", data);
-            });
-
-
-            var queryGetRider = 'SELECT riderid FROM Rides WHERE driverid=' + data.driverID;
-            c.query(queryGetRider, function(err, result, feilds)
-            {
-                if (err) throw err;
-                console.log(result);
-                var rider1 = result[0].riderid;
-                var rider2 = result[1].riderid;
-
-                var returnData = {
-                    'driverID': data.driverID,
-                    'rider1': rider1,
-                    'rider2': rider2
-                };
-                console.log(returnData);
-                io.emit('second rider', returnData);
-            });
-            c.release();
-        })
-
-    });
-
-    //update price and carpool boolean from rides table
-    socket.on('carpool update', function(data) {
-
-    });
+  //update price and carpool boolean from rides table
+  // socket.on('carpool update', function(data) {
+  //
+  // });
 
 });
--- public/js/socketRider.js
+++ public/js/socketRider.js
@@ -2,187 +2,136 @@ var socket = io();
 var map;
 var riderID;
 
-function getMapView(newId)
-{
-    console.log('initialize socket io-client');
-    riderID = newId;
-    console.log(riderID);
-    socket.emit('rider view');
+function getMapView(newId) {
+  console.log('initialize socket io-client');
+  riderID = newId;
+  console.log(riderID);
+  socket.emit('rider view');
 }
 
 //modify this method later to pass the data to insert into rides table
-function notifyDriver(data)
-{
-    console.log('notifying driver');
-    console.log(data);
-    alert("Ride Found, Notifying Your Driver Now");
-    socket.emit('ride request', data);
+function notifyDriver(data) {
+  console.log('notifying driver');
+  console.log(data);
+  alert("Ride Found, Notifying Your Driver Now");
+  socket.emit('ride request', data);
 }
 
-function searchDriver(data)
-{
+function searchDriver(data) {
+  if (data.riderID = riderID) {
     console.log('looking for driver');
     console.log(data);
-    if (markers.length > 0)
-    {
-        var getClosestDriverDataPromise = getClosestDriverData(data);
-        getClosestDriverDataPromise.then(function(result)
-        {
-            console.log('result...');
-            console.log(result);
-            if (result.closestDriverId > 0)
-            {
-                data['closestdriverid'] = result.closestDriverId;
-                socket.emit('find closest', data);
-            }
-            else
-            {
-                alert("No Drivers With Enough Seats");
-                location.reload();
-                return;
-            }
-        });
-    }else{
-    	alert('There is no driver available at the moment');
+    if (markers.length > 0) {
+      var getClosestDriverDataPromise = getClosestDriverData(data);
+      getClosestDriverDataPromise.then(function(result) {
+        console.log('result...');
+        console.log(result);
+        if (result.closestDriverId > 0) {
+          data['closestdriverid'] = result.closestDriverId;
+          socket.emit('find closest', data);
+        } else {
+          alert("No Drivers With Enough Seats");
+          location.reload();
+          return;
+        }
+      });
+    } else {
+      alert('There is no driver available at the moment');
     }
-
+  }
 }
 
-function notifyOthersOfCarpool(data)
-{
-    socket.emit('notify carpool', data);
+function notifyOthersOfCarpool(data) {
+  socket.emit('notify carpool', data);
 }
 
 //approach 2: update database
-function updateSeatIO(data)
-{
-    socket.emit('update seats db', data);
+function updateSeatIO(data) {
+  socket.emit('update seats db', data);
 }
 
-socket.on('update seats', function(data)
-{
+socket.on('update seats', function(data) {
+  removeDriverMarker(data.id);
+  getMapView(riderID);
+});
+socket.on('second rider', function(data) {
+  if (data.rider1 == riderID || data.rider2 == riderID) {
+    console.log("second rider socket socketrider");
+    firstRiderDiscount();
+    alert("There will be a second rider joining you, you will receive $5 off your ride");
+  }
+  removeDriverMarker(data.driverID);
+});
+
+socket.on('map view', function(data) {
+  console.log("got a response")
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    var driverId = data[i].id;
+    var driverLong = data[i].current_long;
+    var driverLat = data[i].current_lat;
+    var driverSeats = data[i].availableseats;
+    console.log(driverId);
+    console.log(driverLong);
+    console.log(driverLat);
+    console.log(driverSeats);
+    removeDriverMarker(driverId);
+    if (driverSeats > 0) {
+      insertNewDriverMarker(driverId, driverLat, driverLong, driverSeats);
+    }
+  }
+});
+
+socket.on('ride completed', function(data) {
+  if (data.riderID = riderID) {
     removeDriverMarker(data.id);
-    getMapView(riderID);
-});
-socket.on('second rider', function(data)
-{
-    if (data.rider1 == riderID || data.rider2 == riderID)
-    {
-        console.log("second rider socket socketrider");
-        firstRiderDiscount();
-        alert("There will be a second rider joining you, you will receive $5 off your ride");
-    }
-    removeDriverMarker(data.driverID);
+    alert('Ride Completed');
+  }
 });
 
-socket.on('map view', function(data)
-{
-    console.log("got a response")
+socket.on('update map', function() {
+  getMapView(riderID);
+});
+
+socket.on('search carpool', function(data) {
+  // console.log(data);
+  if (data[0].carpoolRequesterId == riderID) {
+    // console.log("in search carpool");
+    // console.log(data[0].riderid);
+    // console.log(riderID);
+    // console.log('searching for carpool.....');
+    // console.log("data coming into search carpool");
+    // console.log(data);
+    // console.log(data[0].start_lat);
+    // console.log(data[0].start_long);
+    // console.log(data[0].dest_lat);
+    // console.log(data[0].dest_long);
+    // console.log(data[0].driverid);
+    var checkCarpoolFunctionPromise = checkCarpoolFunction(data[0].start_lat, data[0].start_long, data[0].dest_lat, data[0].dest_long);
+    checkCarpoolFunctionPromise.then(function(result) {
+      console.log(result);
+      if (result) {
+        getRiderInfoCarpool(data);
+      } else {
+        alert("The Riders Available Right Can Only Accept Carpool" +
+          "\nYou Do Not Qualify For Carpooling (Too Far Away) SORRY");
+        location.reload();
+        return;
+      }
+    });
+    // }
+  }
+
+});
+
+socket.on('find nearest', function(data) {
+  // console.log(data);
+  // console.log("data.riderId is " + data.riderID);
+  // console.log("riderID is " + riderID);
+  if (data.riderID == riderID) {
+    console.log('find nearest driver...');
+    console.log('getting rider info data...');
     console.log(data);
-    for (var i = 0; i < data.length; i++)
-    {
-        var driverId = data[i].id;
-        var driverLong = data[i].current_long;
-        var driverLat = data[i].current_lat;
-        var driverSeats = data[i].availableseats;
-        console.log(driverId);
-        console.log(driverLong);
-        console.log(driverLat);
-        console.log(driverSeats);
-        removeDriverMarker(driverId);
-        if (driverSeats > 0)
-        {
-            insertNewDriverMarker(driverId, driverLat, driverLong, driverSeats);
-        }
-    }
-});
-
-socket.on('ride completed', function(data)
-{
-    if (data.riderID = riderID)
-    {
-        removeDriverMarker(data.id);
-        alert('Ride Completed');
-    }
-});
-
-socket.on('update map', function()
-{
-    getMapView(riderID);
-});
-
-
-
-socket.on('search carpool', function(data)
-{
-    console.log("in search carpool");
-    console.log(data[0].riderid);
-    console.log(riderID);
-    if (data[0].riderid != riderID)
-    {
-        console.log('searching for carpool.....');
-        console.log("data coming into search carpool");
-        console.log(data);
-        console.log(data[0].start_lat);
-        console.log(data[0].start_long);
-        console.log(data[0].dest_lat);
-        console.log(data[0].dest_long);
-        console.log(data[0].driverid);
-        var checkCarpoolFunctionPromise = checkCarpoolFunction(data[0].start_lat, data[0].start_long, data[0].dest_lat, data[0].dest_long);
-        checkCarpoolFunctionPromise.then(function(result)
-        {
-            console.log(result);
-            if (result)
-            {
-                getRiderInfoCarpool(data);
-            }
-            else
-            {
-
-                var changedData = {
-                    'driverid': data[0].driverid,
-                    'riderLat': data[0].start_lat,
-                    'riderLng': data[0].start_long,
-                    'destinationLat': data[0].dest_lat,
-                    'destinationLng': data[0].dest_long,
-                    'riderid': data[0].riderid,
-                    'cost': data[0].cost,
-                    'carpool': data[0].carpool,
-                    'duration': data[0].time
-                    // ,
-                    // 'seats': ????
-                };
-
-                var closest = getRiderInfo(changedData);
-            }
-        });
-    }
-
-    // data that comes in data[0].
-    //   driverid: 1000,
-    //   riderid: 1002,
-    //   dest_long: -122.3789554,
-    //   dest_lat: 37.6213129,
-    //   start_long: -121.88107150000002,
-    //   start_lat: 37.3351874,
-    //   cost: 79.63,
-    //   carpool: 0,
-    //   time: 37 } ]
-
-});
-
-socket.on('find nearest', function(data)
-{
-    console.log(data);
-    console.log("data.riderId is " + data.riderID);
-    console.log("riderID is " + riderID);
-    if (data.riderID == riderID)
-    {
-        console.log('find nearest driver...');
-        // setTimeout(function() {
-        console.log('getting rider info data...');
-        console.log(data);
-        var closest = getRiderInfo(data);
-        // },2000);
-    }
+    var closest = getRiderInfo(data);
+  }
 });
